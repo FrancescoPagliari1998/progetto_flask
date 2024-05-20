1c061ffe85b34bc75197e4c5427183a5d4934c69
FrancescoPagliari1998 <francescopagliari520@gmail.com>
2024-05-20 14:26:47 +0200

    ultime modifiche

--- app.py
+++ app.py
@@ -1,8 +1,31 @@
 from flask import Flask, render_template
 import csv
+import mysql.connector
+
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
 
+def create_db_connection():
+    db_config = {
+        'host': app.config['MYSQL_HOST'],
+        'user': app.config['MYSQL_USER'],
+        'password': app.config['MYSQL_PASSWORD'],
+        'database': app.config['MYSQL_DB']
+    }
+    return mysql.connector.connect(**db_config)
+
+def execute_query(query, params=None):
+    connection = create_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    result = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return result
 
 
 @app.route('/')
@@ -16,8 +39,13 @@ def home():
 
 @app.route('/book')
 def book():
-    books = [ { "author": "Yuval Noah Harari", "title": "Sapiens: A Brief History of Humankind", "year": 2011 }, { "author": "Tara Westover", "title": "Educated: A Memoir", "year": 2018 }, { "author": "Cormac McCarthy", "title": "The Road", "year": 2006 } ]
+    books = execute_query('SELECT * FROM book')
     return render_template('book.html', books=books)
+
+@app.route('/api/book')
+def api_book():
+    books = execute_query('SELECT * FROM book')
+    return books
 
 
 
